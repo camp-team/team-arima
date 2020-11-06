660ccdcfeff20a44f2703f1b74c0adac1120b449
hide-a1 <hide210g@gmail.com>
2020-11-07 08:13:11 +0900

    feat(autodeploy): 自動デプロイ

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBH_1hbWap2ebVkOouYM3x49WRaoR2dJX0',
+    authDomain: 'locus-c1684.firebaseapp.com',
+    databaseURL: 'https://locus-c1684.firebaseio.com',
+    projectId: 'locus-c1684',
+    storageBucket: 'locus-c1684.appspot.com',
+    messagingSenderId: '836669020264',
+    appId: '1:836669020264:web:c5e4cd3e50d32fbaa5c3dd',
+    measurementId: 'G-PYB6V3LZCB',
+  },
 };
