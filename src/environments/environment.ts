f34b225e4323247077691f86e1a357b4ffbd9433
hide-a1 <hide210g@gmail.com>
2020-11-06 17:04:14 +0900

    feat(angularfire): init

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyBH_1hbWap2ebVkOouYM3x49WRaoR2dJX0',
+    authDomain: 'locus-c1684.firebaseapp.com',
+    databaseURL: 'https://locus-c1684.firebaseio.com',
+    projectId: 'locus-c1684',
+    storageBucket: 'locus-c1684.appspot.com',
+    messagingSenderId: '836669020264',
+    appId: '1:836669020264:web:c5e4cd3e50d32fbaa5c3dd',
+    measurementId: 'G-PYB6V3LZCB'
+  }
 };
 
 /*
