f639b871c529c742aa9e5f1ccdfb0411eeeece73
chamaboy <hiroponpoko07@gmail.com>
2020-11-07 15:27:14 +0900

    feat(user.service): get関数追加 #6

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,19 +1,33 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
-import { AngularFirestore } from '@angular/fire/firestore';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
 import * as firebase from 'firebase';
+import { Observable, of } from 'rxjs';
+import { shareReplay, switchMap } from 'rxjs/operators';
+import { UserData } from '../interfaces/user-data';
+import { UserService } from './user.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
   uid: string;
+  user$: Observable<UserData> = this.afAuth.authState.pipe(
+    switchMap((afUser) => {
+      if (afUser) {
+        this.uid = afUser?.uid;
+        return this.userService.getUser(this.uid);
+      } else {
+        return of(null);
+      }
+    }),
+    shareReplay(1)
+  );
 
   constructor(
     private afAuth: AngularFireAuth,
-    private db: AngularFirestore,
+    private userService: UserService,
     private router: Router,
     private snackBar: MatSnackBar
   ) {}
