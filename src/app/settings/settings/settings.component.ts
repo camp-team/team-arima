5d1659277f46b9e556e65e9c9baeb32014acd699
chamaboy <hiroponpoko07@gmail.com>
2020-11-07 21:01:18 +0900

    refactor(settings): storageに追加 #21

--- src/app/settings/settings/settings.component.ts
+++ src/app/settings/settings/settings.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
-import { take } from 'rxjs/operators';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Observable } from 'rxjs';
+import { map, take } from 'rxjs/operators';
+import { UserData } from 'src/app/interfaces/user-data';
 import { AuthService } from 'src/app/services/auth.service';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-settings',
@@ -10,29 +14,55 @@ import { AuthService } from 'src/app/services/auth.service';
 })
 export class SettingsComponent implements OnInit {
   oldImageUrl: string;
-
+  selectedImageDataUrl: string;
   maxLength = 60;
+
+  user$: Observable<UserData> = this.authService.user$;
+  uid: string;
 
   nameForm: FormControl = new FormControl('', [
     Validators.required,
     Validators.maxLength(this.maxLength),
   ]);
 
-  constructor(public authService: AuthService) {}
+  constructor(
+    private authService: AuthService,
+    private userService: UserService,
+    private snackBar: MatSnackBar
+  ) {}
 
   ngOnInit(): void {
-    this.authService.user$
+    this.user$.pipe(map((user) => user.uid)).subscribe((id) => (this.uid = id));
+    this.user$
       .pipe(take(1))
       .toPromise()
       .then((user) => {
         this.oldImageUrl = user.avatarUrl || '';
-        console.log(user);
-        console.log(this.oldImageUrl);
         this.nameForm.patchValue(user.name || '');
       });
   }
 
   onCroppedImage(image: string): void {
-    this.oldImageUrl = image;
+    this.selectedImageDataUrl = image;
+  }
+
+  async submit(): Promise<void> {
+    let avatarUrl: string;
+    if (this.selectedImageDataUrl) {
+      avatarUrl = await this.userService.uploadImage(
+        this.selectedImageDataUrl,
+        this.uid
+      );
+    } else {
+      avatarUrl = this.oldImageUrl;
+    }
+    const userData: Omit<UserData, 'likedPostIds'> = {
+      avatarUrl,
+      name: this.nameForm.value,
+      uid: this.uid,
+    };
+    this.userService.updateUser(userData).then(() => {
+      this.snackBar.open('更新しました', null);
+    });
   }
 }
