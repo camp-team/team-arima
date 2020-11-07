7b1586a43c1e744d8420b1a2fc2d775a2753a427
chamaboy <hiroponpoko07@gmail.com>
2020-11-07 18:30:32 +0900

    feat(settings): patchValue実装 #21

--- src/app/settings/settings/settings.component.ts
+++ src/app/settings/settings/settings.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { CropperOptions } from '@deer-inc/cropper';
+import { FormControl, Validators } from '@angular/forms';
+import { take } from 'rxjs/operators';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-settings',
@@ -9,11 +11,28 @@ import { CropperOptions } from '@deer-inc/cropper';
 export class SettingsComponent implements OnInit {
   oldImageUrl: string;
 
-  constructor() {}
+  maxLength = 60;
 
-  ngOnInit(): void {}
+  nameForm: FormControl = new FormControl('', [
+    Validators.required,
+    Validators.maxLength(this.maxLength),
+  ]);
+
+  constructor(public authService: AuthService) {}
+
+  ngOnInit(): void {
+    this.authService.user$
+      .pipe(take(1))
+      .toPromise()
+      .then((user) => {
+        this.oldImageUrl = user.avatarUrl || '';
+        console.log(user);
+        console.log(this.oldImageUrl);
+        this.nameForm.patchValue(user.name || '');
+      });
+  }
 
   onCroppedImage(image: string): void {
-    console.log(image);
+    this.oldImageUrl = image;
   }
 }
