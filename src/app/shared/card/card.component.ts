6cdab4b256685b40333303de98633fa8b3100b47
hide-a1 <hide210g@gmail.com>
2020-11-08 14:22:36 +0900

    feat(carender): carender調整

--- src/app/shared/card/card.component.ts
+++ src/app/shared/card/card.component.ts
@@ -3,6 +3,7 @@ import { Observable } from 'rxjs';
 import { take } from 'rxjs/operators';
 import { Post } from 'src/app/interfaces/post';
 import { UserData } from 'src/app/interfaces/user-data';
+import { AuthService } from 'src/app/services/auth.service';
 import { UserService } from 'src/app/services/user.service';
 
 @Component({
@@ -15,7 +16,10 @@ export class CardComponent implements OnInit {
 
   user$: Observable<UserData>;
 
-  constructor(private userService: UserService) {}
+  constructor(
+    private userService: UserService,
+    public authService: AuthService
+  ) {}
 
   ngOnInit(): void {
     this.user$ = this.userService.getUser(this.post.authorUid).pipe(take(1));
--- src/app/shared/carendar/carendar.component.ts
+++ src/app/shared/carendar/carendar.component.ts
@@ -1,8 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { CalendarOptions } from '@fullcalendar/angular';
-import { Subscription } from 'rxjs';
 import { Post } from 'src/app/interfaces/post';
-import { PostService } from 'src/app/services/post.service';
 
 @Component({
   selector: 'app-carendar',
@@ -11,7 +8,7 @@ import { PostService } from 'src/app/services/post.service';
 })
 export class CarendarComponent implements OnInit {
   @Input() posts: Post[];
-  private subscription = new Subscription();
+
   private readonly preCalendarOption = {
     initialView: 'dayGridMonth',
     weekends: true,
@@ -29,21 +26,19 @@ export class CarendarComponent implements OnInit {
   };
   calendarOptions = this.preCalendarOption;
 
-  constructor(private postService: PostService) {}
+  constructor() {}
 
   ngOnInit(): void {
+    this.calendarOptions = this.preCalendarOption;
     const dates = this.posts.map((post) => {
       return post.createdAt;
     });
     dates.forEach((date) => {
       this.calendarOptions.events.push({
-        date,
-        className: 'done',
-        overlap: false,
-        display: 'background',
+        start: date,
+        end: date,
+        className: 'fc-done',
       });
     });
-    this.calendarOptions.events = [];
-    console.log(this.posts);
   }
 }
